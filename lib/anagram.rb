a9d20dc7b30dbc8eed5332313eee25873ff0a2bc
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-08 20:56:15 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
 # Your code goes here!
+
+require 'pry'
+
+class Anagram
+
+  attr_accessor :anagram
+
+  def initialize(anagram)
+    anagram_split = anagram.split("")
+    #binding.pry
+    @anagram = anagram_split
+  end
+
+  def match(possible_anagrams)
+    confirmed_anagrams = []
+    possible_anagrams.detect do |word|
+      split_word = word.split("")
+      sort_word = split_word.sort
+      if possible_anagrams.detect == @anagram
+        confirmed_anagrams << word
+        binding.pry
+      end
+    end
+    confirmed_anagrams
+  end
+end
