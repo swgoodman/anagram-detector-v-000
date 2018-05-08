f326249ef4f2bf5a292bce4639adcd16e53e057b
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-08 21:44:31 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -15,9 +15,7 @@ class Anagram
     confirmed_anagrams = []
     possible_anagrams.each do |word|
       split_word = word.split("")
-  
       sort_word = split_word.sort
-      binding.pry
       if sort_word == @anagram
         confirmed_anagrams << word
         binding.pry
